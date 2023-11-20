db07790749c6b9bd4389c10c4e8d57de810ded64
Tony Wang <Tony94042@gmail.com>
2023-11-20 20:36:35 +1100

    Annotations

---
+++ src/app/Annotations/variables.ts
@@ -0,0 +1,24 @@
+let apples: number = 5;
+let speed: string = 'fast';
+let hasName: boolean = true;
+let nothingMuch: null = null;
+let nothing: undefined = undefined;
+//Most common annotation
+
+let now: Date = new Date();
+//Built in objects
+
+let colors: string[] = ['Red', 'Blue', 'Red'];
+let myNumbers: number[] = [1,2,3,4];
+//Array
+
+class Car{
+}
+let car: Car = new Car();
+//Classes
+
+let point: { x: number; y: number } = {
+    x: 10,
+    y: 20
+}
+//Object literal
