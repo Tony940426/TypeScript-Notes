04d2b2747fe18616c143d5afb1609f1c469e074b
Tony Wang <Tony94042@gmail.com>
2023-11-22 17:38:17 +1100

    More notes about function annotation

---
+++ src/app/Annotations/object.ts
@@ -0,0 +1,13 @@
+const profile = {
+    name: 'alex',
+    age: 20,
+    cords: {
+        lat: 0,
+        lng: 15
+    }
+    setAge(age: number): void {
+        this.age = age;
+    }
+};
+
+const { cords: {lat, lng}}: {cords: { lat: number, lng: number}} = profile
